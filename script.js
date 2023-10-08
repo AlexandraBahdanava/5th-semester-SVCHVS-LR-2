2a70f93f991c34cd235da61e4adde6f59ea7a13c
Alexandra Bahdanava <bahdanavaalexandra@gmail.com>
2023-10-08 22:16:27 +0300

    feat: fix search errors

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
-const accessKey = 'f40ZxikJqKrZWzZzMK25hA8atAWiatJ2RxoPMS6MUa4'; // Замените на свой API ключ
-const apiUrl = 'https://api.unsplash.com/photos';
+// Функция для получения случайных изображений при загрузке приложения
+function loadRandomImages() {
+    const apiKey = 'f40ZxikJqKrZWzZzMK25hA8atAWiatJ2RxoPMS6MUa4';
+    const apiUrl = `https://api.unsplash.com/photos/random?count=20&client_id=${apiKey}`;
+
+    // Выполнение запроса к API с использованием fetch
+    fetch(apiUrl)
+        .then((response) => response.json())
+        .then((data) => {
+            // Вызываем функцию для отображения результатов
+            displayPhotos(data);
+        })
+        .catch((error) => {
+            console.error('Ошибка при выполнении запроса:', error);
+        });
+}
+
+// Вызываем функцию при загрузке приложения
+window.addEventListener('load', loadRandomImages);
 
 const searchInput = document.getElementById('searchInput');
 const searchButton = document.getElementById('searchButton');
@@ -22,19 +39,43 @@ function performSearch() {
         return;
     }
 
-    fetch(`${apiUrl}?query=${query}`, {
-        headers: {
-            'Authorization': `Client-ID ${accessKey}`
-        }
-    })
-    .then(response => response.json())
-    .then(photos => {
-        displayPhotos(photos);
-    })
-    .catch(error => {
-        console.error('Произошла ошибка при запросе к Unsplash API:', error);
-    });
+    const apiKey = 'f40ZxikJqKrZWzZzMK25hA8atAWiatJ2RxoPMS6MUa4';
+
+    // URL для запроса к Unsplash API
+    const apiUrl = `https://api.unsplash.com/search/photos?query=${query}&per_page=20&client_id=${apiKey}`;
+
+    // Выполнение запроса к API с использованием fetch
+    fetch(apiUrl)
+        .then((response) => response.json())
+        .then((data) => {
+            // Обработка данных и отображение изображений
+            const main = document.querySelector('main');
+            main.innerHTML = '';
+
+            data.results.forEach((photo) => {
+                const photoElement = document.createElement('div');
+                photoElement.classList.add('photo');
+
+                const imgElement = document.createElement('img');
+                imgElement.src = photo.urls.regular;
+                imgElement.alt = photo.alt_description;
+
+                const descriptionElement = document.createElement('p');
+                descriptionElement.textContent = photo.description || 'Без описания';
+
+                // Другие поля можно добавить аналогичным образом
+
+                photoElement.appendChild(imgElement);
+                photoElement.appendChild(descriptionElement);
+                main.appendChild(photoElement);
+            });
+            displayPhotos(data.results);
+        })
+        .catch((error) => {
+            console.error('Ошибка при выполнении запроса:', error);
+        });
 }
+
 
 function clearSearchInput() {
     searchInput.value = '';
